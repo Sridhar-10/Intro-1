d34c630480b16945f5cf1cb61d9c4ded8215ae75
Sridhar-10 <sridharchandhiran@gmail.com>
2022-05-16 19:12:46 +0530

    P-1

---
+++ src/Components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <h1>Mobile Operating System</h1>
+    <ul>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+    </ul>
+    <h1>Mobile Manufactures</h1>
+    <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+    </ul>
+    </>
+  )
+}
+
+export default Card
